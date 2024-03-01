e27e9e61532fceba9d6a277cd1598a57ed4013bb
Orlando Neto <neto020735@gmail.com>
2024-02-29 21:41:46 -0300

    Ajuste na classe ProductController

--- src/main/java/com/devOrlando/springBoot/controller/ProductController.java
+++ src/main/java/com/devOrlando/springBoot/controller/ProductController.java
@@ -49,7 +49,7 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
 
-        productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withSelfRel("Products List"));
+        productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products List"));
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
 
     }
